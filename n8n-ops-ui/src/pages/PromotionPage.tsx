2994af845807af95e032b94b217ff925b89022fa
jkergosi <jasonk@ingensystems.com>
2026-01-02 08:37:16 -0600

    tweaks

---
+++ n8n-ops-ui/src/pages/PromotionPage.tsx
@@ -0,0 +1,5 @@
+// Backwards-compat shim: older code/tests import `PromotionPage`,
+// but the UI has moved to `NewDeploymentPage`.
+export { NewDeploymentPage as PromotionPage } from './NewDeploymentPage';
+
+
